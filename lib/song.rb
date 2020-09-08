8be96a35c871af8ff3cf8d69d05a3644f5896378
Mikel Bishop <mdrew8@gmail.com>
2020-09-08 01:51:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,7 +33,7 @@ class Song
   end 
   
   def self.find_or_create_by_name(title)
-    if self.find_by_name(title).include?(name)
+    if self.find_by_name(title).include?(title)
       name
     else 
       self.create_by_name(title)
